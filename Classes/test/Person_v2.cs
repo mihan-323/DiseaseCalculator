6cf9ce264e0901f76c76aafdec70ba2c3873b7cb
vlsbd <91143641+vlsbd@users.noreply.github.com>
2025-03-30 20:17:51 +0300

    Update Person_v2.cs
    parity with v1 and further improvements

--- Classes/test/Person_v2.cs
+++ Classes/test/Person_v2.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace DiseaseCalculator.Classes
 {
-    class Person2
+    class Person2//rewrite to work with any disease//pack into functions things in main file
     {
-        public readonly string name;
+        publiс string name;
         public readonly bool gender;//true = male
-        public Person2 mother;
-        public Person2 father;
+        public Person2? mother;
+        public Person2? father;
         public List<PersonalDisease> diseases1 = new List<PersonalDisease>();//chromosome 1
         public List<PersonalDisease> diseases2 = new List<PersonalDisease>();//chromosome 2
 
@@ -28,6 +28,7 @@ namespace DiseaseCalculator.Classes
             float mutation_prob = 0.00002f;//спонтанная мутация 0.00002% по Холдейну
             float prob = 0.0f;
             Random rnd = new Random(10);
+            //make a loop through all diseases
             Predicate<PersonalDisease> search = x => x.Equals(new PersonalDisease(Hemophilia.GetHemophiliaInstance(), true));
 
             if (gender == true)
@@ -96,6 +97,10 @@ namespace DiseaseCalculator.Classes
                     {
                         diseases2.Add(new PersonalDisease(Hemophilia.GetHemophiliaInstance(), prob));
                     }
+                    /* //try adding prob to both chromos
+					diseases1.Add(new PersonalDisease(Hemophilia.GetHemophiliaInstance(), prob / 2));
+					diseases2.Add(new PersonalDisease(Hemophilia.GetHemophiliaInstance(), prob / 2));
+					*/
                 }
             }
         }
@@ -107,19 +112,58 @@ namespace DiseaseCalculator.Classes
             {
                 foreach (var item in diseases1)
                 {
-                    pdiseases += item.ToString();
+                    pdiseases += "\n  " + item.ToString() + " (" + item.calculated_probability + ")";
                 }
                 foreach (var item in diseases2)
                 {
-                    pdiseases += item.ToString();
+                    pdiseases += "\n  " + item.ToString() + " (" + item.calculated_probability + ")";
                 }
             }
             else
             {
-                pdiseases = "здоров";
+                if (gender == true)
+                {
+                    pdiseases = "\n  Здоров";
+                }
+                else
+                {
+                    pdiseases = "\n  Здорова";
+                }
             }
 
             return name + " " + (gender ? "M" : "F") + " " + pdiseases;
+        }
+
+        public void ChangeName(String s)
+        {
+            name = s;
+        }
+
+        // для проверки работоспособности
+        public bool SearchHemophilia()
+        {
+            Predicate<PersonalDisease> search = x => x.Equals(new PersonalDisease(Hemophilia.GetHemophiliaInstance(), true));
+            return diseases.FindAll(search).Count > 0;
+        }
+
+        // для проверки работоспособности
+        public void AddHemophilia()
+        {
+            if (SearchHemophilia())
+                throw new Exception("Болезнь уже добавлена");
+
+            // 100%?
+            diseases.Add(new PersonalDisease(Hemophilia.GetHemophiliaInstance(), true));
+        }
+
+        // для проверки работоспособности
+        public void RemoveHemophilia()
+        {
+            if (!SearchHemophilia())
+                throw new Exception("Болезнь не найдена");
+
+            // 100%?
+            diseases.remove();///------
         }
     }
 }
